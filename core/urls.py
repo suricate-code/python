035b8e533de550016d832b7ef27a37cae856cc23
súri <savalisch34@gmail.com>
2026-02-19 18:17:49 -0300

    fix:melhoria

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index, contato, produtos,clientes
+from .views import index, contato, produtos,clientes,cadastraClientes,salvaClientes
 
 urlpatterns = [
     path('', index, name="urlindex"),
     path('contato', contato, name="urlcontato"),
     path('produtos', produtos, name="urlprodutos"),
-    path('clientes', clientes, name="urlclientes")
+    path('clientes', clientes, name="urlclientes"),
+    path('cadastraClientes', cadastraClientes, name="urlcadastraClientes"),
+    path('salvaClientes', salvaClientes,name = "urlsalvaClientes")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import Produto,Cliente
 
+from .models import Produto,Cliente
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Cliente
 def index(request):
     context = {'curso': 'Desenvolvimento de Sistemas'}
     return render(request, 'index.html', context)
@@ -22,3 +24,26 @@ def clientes(request):
     clientes = Cliente.objects.all()
     context = {'cli': clientes}
     return render(request, 'clientes.html', context)
+
+def cadastraClientes(request):
+    return render(request, 'cadastraClientes.html')
+    
+def salvaClientes(request):
+    if request.method == "POST":
+        thisNome = request.POST.get('nome')
+        thisSobrenome = request.POST.get('sobrenome')
+        thisEmail = request.POST.get('email')
+        thisCpf = request.POST.get('cpf')
+        thisTelefone = request.POST.get('telefone')
+
+        cliente = Cliente(
+            nome=thisNome,
+            sobrenome=thisSobrenome,
+            email=thisEmail,
+            cpf=thisCpf,
+            telefone=thisTelefone
+        )
+
+        cliente.save()
+
+        return redirect('urlclientes')
